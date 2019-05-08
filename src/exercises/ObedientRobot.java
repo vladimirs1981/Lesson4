f4bf7663d84a32ee3e8a8207cd29b2673dd15692
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-05-08 19:11:23 +0200

    ObedientRobot

--- src/exercises/ObedientRobot.java
+++ src/exercises/ObedientRobot.java
@@ -2,19 +2,51 @@ package exercises;
 
 import javax.swing.JOptionPane;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class ObedientRobot {
+	Robot ORobot = new Robot("mini");
+
+	private void drawCircle() {
+		ORobot.penDown();
+		ORobot.setSpeed(10);
+		for (int i = 0; i < 360; i++) {
+			ORobot.turn(1);
+			ORobot.move(1);
+		}
+	}
+
+	private void drawSqare() {
+		ORobot.penDown();
+		ORobot.setSpeed(10);
+		for (int i = 0; i < 4; i++) {
+			ORobot.move(90);
+			ORobot.turn(90);
+		}
+	}
+
+	private void drawTriangle() {
+		ORobot.penDown();
+		ORobot.setSpeed(10);
+		for (int i = 0; i < 3; i++) {
+			ORobot.move(150);
+			ORobot.turn(120);
+		}
+
+	}
+
 
 	public static void main(String[] args) {
 		int shape = JOptionPane.showOptionDialog(null, "Which shape would you like me to draw?", "Obedient Robot", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Circle", "Square", "Triangle" }, null);
-		/*
-		 * Create methods to draw a circle, square and triangle and call the appropriate
-		 * one depending on the user choice.
-		 */
-		
-		
+		if (shape == 1) {
+			new ObedientRobot().drawSqare();
+		} else if (shape == 2) {
+			new ObedientRobot().drawTriangle();
+		} else if (shape == 0) {
+			new ObedientRobot().drawCircle();
+		}
+
 	}
-	
-	
 
 }
