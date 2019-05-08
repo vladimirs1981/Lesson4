08e7c982cc148e0d88856cb92c80eca64f743689
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-05-08 19:31:53 +0200

    FourSquare

--- src/optional/FourSquare.java
+++ src/optional/FourSquare.java
@@ -2,32 +2,37 @@ package optional;
 
 import javax.swing.JOptionPane;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class FourSquare {
-	
-	// 2. Create a new Robot
-	
+
+	Robot squary = new Robot("mini");
 
 	void go() {
-		// 4. Make the robot move as fast as possible
 
-		// 5. Set the pen width to 5
+		squary.setSpeed(10);
 
-		// 6. Do steps #7 to #8 four times...
+		squary.setPenWidth(5);
 
-			// 7. Set the pen color to random
-	
-			// 1. Call the drawSquare() method
-	
-			// 8. Turn the robot 90 degrees to the right
+		for (int i = 0; i < 4; i++) {
+
+			squary.setRandomPenColor();
+
+			drawSquare();
+
+			squary.turn(90);
+		}
 
 	}
 
-	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		squary.penDown();
+		for (int i = 0; i < 4; i++) {
 
-		
-		
+			squary.turn(90);
+			squary.move(70);
+		}
+
 	}
 
 	public static void main(String[] args) {
