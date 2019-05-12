312ddbdb9d9dbbdb1a30f64264b6f70b0dd799f6
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-05-12 19:57:50 +0200

    EmailSender

--- src/exercises/EmailSender.java
+++ src/exercises/EmailSender.java
@@ -2,24 +2,30 @@ package exercises;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class EmailSender {
 
 	void go() {
-		/*
-		 * Use the methods below to send an email and print a warning if the email did
-		 * not send correctly.
-		 */
-
 		
+		String emailaddress = JOptionPane.showInputDialog(null, "Enter email address:");
+		setEmailAddress(emailaddress);
+		String message = JOptionPane.showInputDialog(null, "Enter message");
+		if (send(message)) {
+			JOptionPane.showMessageDialog(null, "Message sent");
+		} else {
+			JOptionPane.showMessageDialog(null, "Message not sent");
+		}
 	}
 
-	/* Returns true if email was sent successfully, false if it failed to send. */
+	
 	boolean send(String greeting) {
 		if (this.emailAddress.isBlank()) {
 			System.err.println("You did not provide a recipient for the email!");
 			return false;
 		}
 		return new Random().nextBoolean();
+
 	}
 
 	private String emailAddress = "";
